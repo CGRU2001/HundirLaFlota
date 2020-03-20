18b278257039c2fbb8c4f5fc28f24efbdaab7874
Carlos <carlosguillermoru@gmail.com>
2020-03-20 21:47:59 +0100

    Implementa la clase mapa

---
+++ src/main/java/carlos/Mapa.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package carlos;
+
+/**
+ *
+ * @author carlos
+ */
+public class Mapa {
+    private Barco[][] dimensiones;
+    public Mapa(int width, int height){
+        this.dimensiones = new Barco[width][height];
+    }
+
+    
+}
