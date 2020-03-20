ea2660fb95c9ab5fe08f9949ed1e391249eafb8a
Carlos <carlosguillermoru@gmail.com>
2020-03-20 21:54:37 +0100

    Implementa la clase jugador

---
+++ src/main/java/carlos/Jugador.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package carlos;
+
+/**
+ *
+ * @author carlos
+ */
+public class Jugador {
+    public Mapa mapa;
+    public String nombre;
+
+    public Jugador(String nombre) {
+        this.nombre = nombre;
+        mapa = new Mapa(25, 25);
+        for (int i = 0; i < mapa.getDimensiones().length; i++) {
+            for (int j = 0; j < mapa.getDimensiones()[i].length; j++) {
+                mapa.getDimensiones()[i][j] = new Barco();
+            }
+        }
+    }
+    
+    
+    
+}
